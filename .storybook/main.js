0cd5212ee21c912c462138773035e9447dcc770b
eliasmelgaco <eliasmelgaco@gmail.com>
2020-11-11 09:58:08 -0300

    add storybook and change to exact packages version

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ['../src/components/**/stories.tsx'],
+  addons: [
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-links',
+    '@storybook/addon-actions'
+  ]
+};

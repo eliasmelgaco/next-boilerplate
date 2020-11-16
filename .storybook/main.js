eecec18ca15c6f86e5b6c7c7600f047ef40ba9f2
eliasmelgaco <eliasmelgaco@gmail.com>
2020-11-16 08:43:01 -0300

    update storybook to 6

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,4 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
-  addons: [
-    '@storybook/addon-knobs/register',
-    '@storybook/addon-links',
-    '@storybook/addon-actions'
-  ]
+  addons: ['@storybook/addon-essentials']
 };
